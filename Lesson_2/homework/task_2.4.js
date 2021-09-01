a54fde81689ebfd16807b528cebb112cfa7c25a4
Dmitry <myxahev2@gmail.com>
2021-09-01 11:04:24 +0500

    complete task_2.4

---
+++ Lesson_2/homework/task_2.4.js
@@ -0,0 +1,39 @@
+/**
+ * Функция складывает переданные параметры.
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {number}
+ */
+function addition(a, b) {
+    return a + b;
+}
+
+/**
+ * Функция вычетает из первого переданого параметра второе.
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {number}
+ */
+function subtraction(a, b) {
+    return a - b;
+}
+
+/**
+ * Функция делит первый переданый параметр на второй.
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {number}
+ */
+function division(a, b) {
+    return a / b;
+}
+
+/**
+ * Функция умножает переданные параметры.
+ * @param {number} a 
+ * @param {number} b 
+ * @returns {number}
+ */
+function multiplication(a, b) {
+    return a * b;
+}
